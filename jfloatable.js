cdfd77c88d7e4755fd5569f76fdd314c8ddb9bd8
Roberto Cataneo <roberto.cataneo@crowdint.com>
2010-11-20 21:22:53 -0800

    Included shadow effect

--- jfloatable.js
+++ jfloatable.js
@@ -4,7 +4,7 @@
     Author:       Roberto Cataneo - http://github.com/rottgoth
     Version:      0.1.beta
     License:      GPL - http://www.opensource.org/licenses/gpl-license.php
-    Source Code:  Github - https://github.com/rottgoth/jfloatable
+    Source Code:  Github - https://github.com/crowdint/jfloatable
 
 */
 
@@ -14,7 +14,9 @@
   var defaults = {
    container: '',
    marginTop: 0,
-   marginBottom: 0
+   marginBottom: 0,
+   with_shadow: true,
+   shadow_values: '3px 3px 10px'
   };
 
   var options = $.extend(defaults, options);
@@ -31,10 +33,12 @@
           $(this).data('parent').removeClass('floatable-top');
           $(this).addClass('floatable-fixed');
           $(this).css({'position': 'fixed', 'top': '0'});
+          if($(this).data('with_shadow'))
+            $(this).css({'-moz-box-shadow': $(this).data('shadow_values'), '-webkit-box-shadow': $(this).data('shadow_values'), 'box-shadow': $(this).data('shadow_values')});
         } else {
           $(this).removeClass('floatable-fixed');
           $(this).data('parent').addClass('floatable-top');
-          $(this).css({'position': 'absolute', 'top': '0', 'bottom': 'auto'});
+          $(this).css({'position': 'absolute', 'top': '0', 'bottom': 'auto', '-moz-box-shadow': 'none', '-webkit-box-shadow': 'none', 'box-shadow': 'none'});
           $(this).data('parent').css({'position': 'absolute', 'top': '0', 'bottom': 'auto'});
         }
       } else {
@@ -43,20 +47,22 @@
             $(this).data('parent').removeClass('floatable-bottom');
             $(this).addClass('floatable-fixed');
             $(this).css({'position': 'fixed', 'top': '0', 'bottom': 'auto'});
+            if($(this).data('with_shadow'))
+              $(this).css({'-moz-box-shadow': $(this).data('shadow_values'), '-webkit-box-shadow': $(this).data('shadow_values'), 'box-shadow': $(this).data('shadow_values')});
           }
         } else {
           if($(this).data('window_top') < $(this).data('image_top')) {
             $(this).data('parent').addClass('floatable-top');
             $(this).data('parent').removeClass('floatable-bottom');
             $(this).removeClass('floatable-fixed');
-            $(this).css({'position': 'absolute', 'top': '0', 'bottom': 'auto'}); //maybe add bottom
+            $(this).css({'position': 'absolute', 'top': '0', 'bottom': 'auto', '-moz-box-shadow': 'none', '-webkit-box-shadow': 'none', 'box-shadow': 'none'});
             $(this).data('parent').css({'position': 'absolute', 'top': '0', 'bottom': 'auto'});
           } else {
             if($(this).data('bottom_limit') <= $(this).data('current_img_bottom')) {
               $(this).data('parent').addClass('floatable-bottom');
               $(this).data('parent').removeClass('floatable-top');
               $(this).removeClass('floatable-fixed');
-              $(this).css({'position': 'absolute', 'top': 'auto', 'bottom': '0'});
+              $(this).css({'position': 'absolute', 'top': 'auto', 'bottom': '0', '-moz-box-shadow': 'none', '-webkit-box-shadow': 'none', 'box-shadow': 'none'});
               $(this).data('parent').css({'position': 'absolute', 'top': 'auto', 'bottom': '0'});
             }
           }
@@ -81,6 +87,8 @@
     container.css({'position': 'relative'});
     obj.data('parent',parent);
     obj.data('container',container);
+    obj.data('with_shadow', (options.with_shadow == 'true' || options.with_shadow == true ? true : false));
+    obj.data('shadow_values', options.shadow_values);
     
     obj.data('bottom_limit', parseInt($(container).offset().top + $(container).height()));
     obj.data('image_top', parseInt($(obj).offset().top - options.marginTop));
